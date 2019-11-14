3819cf1ba5d18cda12ff9c0522f126a079bba166
Arthur BLONDEAU <arthur.blondeau@etu.u-bordeaux.fr>
2019-11-14 09:56:56 +0100

    Modification du .gitignore et correction du game.c

--- game.c
+++ game.c
@@ -1,25 +1,20 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "game.h"
 #define SIZE 12
 
-typedef struct game_s{
+struct game_s{
     color *tab; //le tableau contenant les cases du jeux (2D ?)
     unsigned int nb_moves_max; // nombre de coups max
     int current_moves; //nombre de coups actuels
     color *tab_init;
     unsigned int size;
-}*game;
-
-typedef enum color_e {RED, GREEN, BLUE, YELLOW, NB_COLORS} color;
+};
 
 
-//jeux en mémoire
-typedef const struct game_s{
-    /*color *tab; //le tableau contenant les cases du jeux
-    unsigned int nb_moves_max; // nombre de coups max
-    int current_moves; //nombre de coups actuels*/
-}*cgame;
+enum color_e {RED, GREEN, BLUE, YELLOW, NB_COLORS};
+
 
 game game_new(color *cells, unsigned int nb_moves_max){return;}
 
@@ -29,17 +24,26 @@ void game_set_cell_init(game g, unsigned int x, unsigned int y, color c){}
 
 void game_set_max_moves(game g, unsigned int nb_max_moves){}
 
-unsigned int game_nb_moves_max(cgame g){return 0;}
+unsigned int game_nb_moves_max(cgame g){
+    if (g == NULL) exit(EXIT_FAILURE);
+    return g->nb_moves_max;
+}
 
 color game_cell_current_color(cgame g, unsigned int x, unsigned int y){return BLUE;}
 
-unsigned int game_nb_moves_cur(cgame g){return 0;}
+unsigned int game_nb_moves_cur(cgame g){
+    if (g == NULL) exit(EXIT_FAILURE);
+
+}
 
 void game_play_one_move(game g, color c){} //flood fill algo
 
 game game_copy(cgame g){return;}
 
-void game_delete(game g){}
+void game_delete(game g){
+    if (g == NULL) exit(EXIT_FAILURE);
+    free(g);
+}
 
 bool game_is_over(cgame g){return true;}
 
