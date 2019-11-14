c59090d1bc65f47a041a195e4e6753dd603d0032
charles goedefroit <charles.goedefroit@etu.u-bordeaux.fr>
2019-11-14 10:36:20 +0100

    game new fini?

--- game.c
+++ game.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "game.h"
-#define SIZE 12
+#define SIZE 12 //TODO: voire pour le suppriemer
 
 struct game_s{
     color *tab; //le tableau contenant les cases du jeux (2D ?)
@@ -16,7 +16,41 @@ struct game_s{
 enum color_e {RED, GREEN, BLUE, YELLOW, NB_COLORS};
 
 
-game game_new(color *cells, unsigned int nb_moves_max){return;}
+game game_new(color *cells, unsigned int nb_moves_max) {
+    if (cells == NULL) {
+        fprintf(stderr, "Bad parameter");
+        exit(EXIT_FAILURE);
+    }
+
+    game_s *g = malloc(sizeof(game_s));
+    if (g == NULL) {
+        fprintf(stderr, "Not enough memory");
+        exit(EXIT_FAILURE);
+    }
+
+    g->nb_moves_max = nb_moves_max;
+    g->current_moves = 0;
+    g->size = SIZE;
+
+    g->tab = malloc((SIZE*SIZE)*sizeof(color));
+    if (g->tab == NULL) {
+        fprintf(stderr, "Not enough memory");
+        exit(EXIT_FAILURE);
+    }
+
+    g->tab_init = malloc((SIZE * SIZE) * sizeof(color));
+    if (g->tab_init == NULL) {
+        fprintf(stderr, "Not enough memory");
+        exit(EXIT_FAILURE);
+    }
+
+    for (uint i = 0; i < SIZE*SIZE; i++) {
+        g->tab[i] = cells[i];
+        g->tab_init[i] = cells[i];
+    }
+
+    return g;
+}
 
 game game_new_empty(){return;}
 
@@ -47,4 +81,4 @@ void game_delete(game g){
 
 bool game_is_over(cgame g){return true;}
 
-void game_restart(game g){} 
+void game_restart(game g){}
