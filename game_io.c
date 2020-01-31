2448c6e90573d43df428747cb2ea6067a9baf90c
Victor Andrault <victor.andrault@etu.u-bordeaux.fr>
2020-01-31 15:59:01 +0100

    ajout de la fonction game_save dans game_io.c

--- game_io.c
+++ game_io.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "game.h"
+#include <stdlib.h>
 
 /**
  * @brief load a new game from a file
@@ -10,12 +11,22 @@
  */
 game game_load(char *filename) { return NULL; }
 
-/**
- * @brief save the game
- *
- * @param g
- * @param filename name of new save file
- * @pre @p g != NULL
- * @pre @p filename != NULL
- */
-void game_save(cgame g, char *filename) {}
+
+void game_save(cgame g, char *filename) {
+    if (g==NULL){
+        printf("Invalid cgame pointer");
+        exit(EXIT_FAILURE);
+    }
+    FILE* savefile = NULL;
+    savefile = fopen(filename,"w");
+    if (savefile != NULL){
+        fprintf (savefile, "%u %u %u %b\n", game_width(g), game_height(g), game_nb_moves_max(g), game_is_wrapping(g));
+        for (int y=0; y<game_height(g); y++){
+            for (int x = 0; x<game_width(g); x++){
+                fprintf(savefile,"%u", game_cell_current_color(g , x, y));
+            }
+            fprintf(savefile, "\n");
+        }
+        fclose(savefile);
+    }
+}
