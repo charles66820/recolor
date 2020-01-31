483ed7c65109ff28d8e83446be9e286c18d43061
Victor Andrault <victor.andrault@etu.u-bordeaux.fr>
2020-01-31 19:00:50 +0100

    correction de game_save

--- game_io.c
+++ game_io.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "game.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * @brief load a new game from a file
@@ -11,22 +12,32 @@
  */
 game game_load(char *filename) { return NULL; }
 
+void game_save(cgame g, char *name) {
+  if (g == NULL || name == NULL) {
+    printf("At least one of the pointers is invalid");
+    exit(EXIT_FAILURE);
+  }
 
-void game_save(cgame g, char *filename) {
-    if (g==NULL){
-        printf("Invalid cgame pointer");
-        exit(EXIT_FAILURE);
+  // Creation of the name of the file
+  char *filename = malloc(strlen(name) + 4);
+  strcat(filename, name);
+  strcat(filename, ".txt");
+  FILE *savefile = NULL;
+  savefile = fopen(filename, "w");
+  if (savefile == NULL) {
+    printf("The file couldn't be created");
+    exit(EXIT_FAILURE);
+
+  // Writting of the parameters of the game in the file
+  fprintf(savefile, "%u %u %u %d\n", game_width(g), game_height(g),
+          game_nb_moves_max(g), game_is_wrapping(g));
+  // Writting of the table of the game in the file
+  for (int y = 0; y < game_height(g); y++) {  //
+    for (int x = 0; x < game_width(g); x++) {
+      fprintf(savefile, "%u ", game_cell_current_color(g, x, y));
     }
-    FILE* savefile = NULL;
-    savefile = fopen(filename,"w");
-    if (savefile != NULL){
-        fprintf (savefile, "%u %u %u %b\n", game_width(g), game_height(g), game_nb_moves_max(g), game_is_wrapping(g));
-        for (int y=0; y<game_height(g); y++){
-            for (int x = 0; x<game_width(g); x++){
-                fprintf(savefile,"%u", game_cell_current_color(g , x, y));
-            }
-            fprintf(savefile, "\n");
-        }
-        fclose(savefile);
-    }
+    fprintf(savefile, "\n");
+  }
+
+  fclose(savefile);
 }
