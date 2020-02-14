d6e85e800809041b0e01eae4c90d0f4e76767b8a
Victor Andrault <victor.andrault@etu.u-bordeaux.fr>
2020-02-14 12:22:39 +0100

    création de solution.c

---
+++ solution.c
@@ -0,0 +1,14 @@
+#include "game.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "solution.h"
+
+struct solution_s{
+    color* tab;
+    uint tab_len;
+}solution;
+
+
+
+solution find_one()
