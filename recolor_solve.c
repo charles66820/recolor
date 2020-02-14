37cc083c1eaa3f4df00470ce1013ce1481b52940
Victor Andrault <victor.andrault@etu.u-bordeaux.fr>
2020-02-14 11:14:17 +0100

    enlevation des #include inutiles dans recolor_solve.c et ajout du target_link_libraries de recolor_solve dans CMakeLists.txt

--- recolor_solve.c
+++ recolor_solve.c
@@ -1,9 +1,6 @@
-#define _GNU_SOURCE
 #include "game.h"
-#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 
 int main(int* argc, int* argv[]){}
