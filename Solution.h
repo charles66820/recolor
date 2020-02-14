1431909dbbe9fa061840a8e9a34a8d7fb7b42eac
Arthur BLONDEAU <arthur.blondeau@etu.u-bordeaux.fr>
2020-02-14 12:22:43 +0100

    Création de Solution.h

---
+++ Solution.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "game.h"
+
+typedef struct solution_s *solution;
+
+solution find_one(cgame g);
+
+void nb_sol(cgame g);
+
+void find_min (cgame g);
