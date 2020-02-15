6749c2a8d8c00c0099db77c1fe772b2372c03792
= <arblondeau@etu.u-bordeaux.fr>
2020-02-15 13:44:16 +0100

    Ajout de la doc dans solution.h (+renommé Solution.h en solution.h)

--- Solution.h
+++ Solution.h
@@ -3,10 +3,29 @@
 #include <string.h>
 #include "game.h"
 
+/**
+ * @brief The structure pointer that stores the solution. it willbe useful to
+ *transfer those data in a file.
+ **/
 typedef struct solution_s *solution;
 
+/**
+ * @brief find one possible solution and store it in the struct solution
+ *
+ * @param g game with cells to print
+ */
 solution find_one(cgame g);
 
+/**
+ * @brief seak for the number of solution
+ *
+ * @param g game with cells to print
+ */
 void nb_sol(cgame g);
 
-void find_min (cgame g);
+/**
+ * @brief find the solution who require the smallest amount of moves
+ *
+ * @param g game with cells to print
+ */
+void find_min(cgame g);
