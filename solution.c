5c952c997a348c9aa651a2231a9510a627f5f418
Arthur BLONDEAU <arthur.blondeau@etu.u-bordeaux.fr>
2020-02-21 15:43:20 +0100

    Mise en place des fonctions dans solution.c

--- solution.c
+++ solution.c
@@ -1,16 +1,31 @@
+#include "solution.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "solution.h"
 
-struct solution_s{
-    color* tab;
-    uint tab_len;
+struct solution_s {
+  uint* tab;
+  uint tab_len;
 };
 
+uint solution_len(solution sol) {
+  uint length = sol->tab_len;
+  return length;
+}
 
+char* solution_string(solution sol) {
+  char* string;
+  for (uint i = 0; i < sol->tab_len; i++) {
+    sprintf(string, "%d", sol->tab[i]);
+  }
+  return string;
+}
 
+solution create_solution(uint* tab, uint length) {
+  solution sol = malloc(sizeof(struct solution_s));
+  sol->tab = tab;
+  sol->tab_len = length;
+  return sol;
+}
 
-
-
-
+void delete_solution(solution sol);
