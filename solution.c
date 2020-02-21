609003d8600d648d79d09c9db9d9f93797af1d3c
Arthur BLONDEAU <arthur.blondeau@etu.u-bordeaux.fr>
2020-02-21 15:52:20 +0100

    Ajout de delete_solution et check_pointer et renommage de solution_len et solution_string en len_solution et string_solution

--- solution.c
+++ solution.c
@@ -8,12 +8,24 @@ struct solution_s {
   uint tab_len;
 };
 
-uint solution_len(solution sol) {
+void check_pointer(const void* p, char* msg) {
+  if (p == NULL) {
+    if (msg == NULL)
+      fprintf(stderr, "Null pointer error.\n");
+    else
+      fprintf(stderr, "%s\n", msg);
+    exit(EXIT_FAILURE);
+  }
+}
+
+uint len_solution(solution sol) {
+  check_pointer(sol, "sol parameter on the function len_solution is null.");
   uint length = sol->tab_len;
   return length;
 }
 
-char* solution_string(solution sol) {
+char* string_solution(solution sol) {
+  check_pointer(sol, "sol parameter on the function string_solution is null.");
   char* string;
   for (uint i = 0; i < sol->tab_len; i++) {
     sprintf(string, "%d", sol->tab[i]);
@@ -22,10 +34,23 @@ char* solution_string(solution sol) {
 }
 
 solution create_solution(uint* tab, uint length) {
+  check_pointer(sol, "sol parameter on the function create_solution is null.");
   solution sol = malloc(sizeof(struct solution_s));
   sol->tab = tab;
   sol->tab_len = length;
   return sol;
 }
 
-void delete_solution(solution sol);
+void delete_solution(solution sol) {
+  if (sol != NULL) {
+    if (sol->tab != NULL) {
+      for (uint i = 0; i < sol->tab_len; i++) {
+        free(sol->tab[i])
+      }
+      free(sol->tab);
+    }
+    if (sol->tab_len != NULL) {
+      free(sol->tab_len);
+    }
+  }
+}
