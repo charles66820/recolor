a7e2495155a166b7e809f84a5309bdbccf04737f
charles goedefroit <charles.goedefroit@etu.u-bordeaux.fr>
2020-03-29 19:32:28 +0200

    up

--- recolor_sdl.c
+++ recolor_sdl.c
@@ -11,10 +11,22 @@
 #define FONTSIZE 36
 #define BACKGROUND "assets/nodejs.svg"  // TEMP: change with real bg
 #define ICON "assets/129Magikarp.png"   // TEMP: change with real icon
+#define SHADOWBOX0 "assets/shadowBox0.png"
+#define SHADOWBOX1 "assets/shadowBox1.png"
+#define SHADOWBOX2 "assets/shadowBox2.png"
+#define SHADOWBOX3 "assets/shadowBox3.png"
 
 struct Env_t {
   SDL_Texture* background;
   SDL_Surface* icon;
+  SDL_Surface* sShadowBox0;
+  SDL_Surface* sShadowBox1;
+  SDL_Surface* sShadowBox2;
+  SDL_Surface* sShadowBox3;
+  SDL_Texture* shadowBox0;
+  SDL_Texture* shadowBox1;
+  SDL_Texture* shadowBox2;
+  SDL_Texture* shadowBox3;
 };
 
 Env* init(SDL_Window* win, SDL_Renderer* ren, int argc, char* argv[]) {
@@ -34,10 +46,37 @@ Env* init(SDL_Window* win, SDL_Renderer* ren, int argc, char* argv[]) {
     SDL_ShowSimpleMessageBox(0, "IMG_Load", SDL_GetError(), win);
   }
 
+  // Load shadows
+  env->sShadowBox0 = IMG_Load(SHADOWBOX0);
+  if (!env->sShadowBox0) {
+    ERROR("IMG_Load: %s\n", ICON);
+    SDL_ShowSimpleMessageBox(0, "IMG_Load", SDL_GetError(), win);
+  }
+  env->shadowBox0 = SDL_CreateTextureFromSurface(ren, env->sShadowBox0);
+
+  env->sShadowBox1 = IMG_Load(SHADOWBOX1);
+  if (!env->sShadowBox1) {
+    ERROR("IMG_Load: %s\n", ICON);
+    SDL_ShowSimpleMessageBox(0, "IMG_Load", SDL_GetError(), win);
+  }
+  env->shadowBox1 = SDL_CreateTextureFromSurface(ren, env->sShadowBox1);
+
+  env->sShadowBox2 = IMG_Load(SHADOWBOX2);
+  if (!env->sShadowBox2) {
+    ERROR("IMG_Load: %s\n", ICON);
+    SDL_ShowSimpleMessageBox(0, "IMG_Load", SDL_GetError(), win);
+  }
+  env->shadowBox2 = SDL_CreateTextureFromSurface(ren, env->sShadowBox2);
+
+  env->sShadowBox3 = IMG_Load(SHADOWBOX3);
+  if (!env->sShadowBox3) {
+    ERROR("IMG_Load: %s\n", ICON);
+    SDL_ShowSimpleMessageBox(0, "IMG_Load", SDL_GetError(), win);
+  }
+  env->shadowBox3 = SDL_CreateTextureFromSurface(ren, env->sShadowBox3);
+
   // Set icon
   SDL_SetWindowIcon(win, env->icon);
-
-
 
   return env;
 }
@@ -48,7 +87,7 @@ void render(SDL_Window* win, SDL_Renderer* ren, Env* env) {
   SDL_RenderClear(ren);
 
   // Draw background image
-  SDL_RenderCopy(ren, env->background, NULL, NULL);
+  // SDL_RenderCopy(ren, env->background, NULL, NULL);
 
   // Local vars
   int winW = SCREEN_WIDTH;
@@ -57,22 +96,26 @@ void render(SDL_Window* win, SDL_Renderer* ren, Env* env) {
   SDL_GetWindowSize(win, &winW, &winH);
 
   int xMarging = winW * 8 / 100;
-  int yMarging = winH * 8 / 100;
+  int yMarging = winH * 4 / 100;
 
-  // Draw div rectangle
-  int divW = winW - xMarging * 2;
-  int divH = winH - yMarging * 2;
+  // Draw rounded surface for the color grid
+  int gridMaxW = winW - xMarging * 2;
+  int gridMaxH = winH - yMarging * 4;
 
-  SDL_Rect rec = {xMarging, yMarging, divW, divH};
-  SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
-  SDL_RenderDrawRect(ren, &rec);
-  SDL_RenderFillRect(ren, &rec);
+  int gridSize = gridMaxW > gridMaxH ? gridMaxH : gridMaxW;
+
+  SDL_Rect rec = {(winW - gridSize) / 2, (winH - yMarging * 3 - gridSize) / 2,
+                  gridSize, gridSize};
+  SDL_RenderCopy(ren, env->shadowBox1, NULL, &rec);
+
+  // Draw grid
+  // SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+  // SDL_RenderDrawRect(ren, &rec);
+  // SDL_RenderFillRect(ren, &rec);
 
   // Draw line
-  SDL_SetRenderDrawColor(ren, 42, 42, 42, 255);
-  SDL_RenderDrawLine(ren, 0, winH - yMarging, winW, winH - yMarging);
-
-
+  SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+  SDL_RenderDrawLine(ren, 0, winH - yMarging * 3, winW, winH - yMarging * 3);
 }
 
 bool process(SDL_Window* win, SDL_Renderer* ren, Env* env, SDL_Event* e) {
@@ -86,5 +129,13 @@ bool process(SDL_Window* win, SDL_Renderer* ren, Env* env, SDL_Event* e) {
 void clean(SDL_Window* win, SDL_Renderer* ren, Env* env) {
   SDL_DestroyTexture(env->background);
   SDL_FreeSurface(env->icon);
+  SDL_FreeSurface(env->sShadowBox0);
+  SDL_FreeSurface(env->sShadowBox1);
+  SDL_FreeSurface(env->sShadowBox2);
+  SDL_FreeSurface(env->sShadowBox3);
+  SDL_DestroyTexture(env->shadowBox0);
+  SDL_DestroyTexture(env->shadowBox1);
+  SDL_DestroyTexture(env->shadowBox2);
+  SDL_DestroyTexture(env->shadowBox3);
   free(env);
 }
